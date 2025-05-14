b1ef070d4cf5ee3b0eafaea9dc360ca50b7292ee
Nico Ledda <nicoledda99@gmail.com>
2025-05-15 00:05:41 +0200

    Exercise_2 homework 5 integrated custom exceptions into Profile (constructor and setter)

--- src/ap_homework_5/exercise_2/Profile.java
+++ src/ap_homework_5/exercise_2/Profile.java
@@ -2,18 +2,22 @@ package ap_homework_5.exercise_2;
 
 public class Profile {
     private ShippingInfo shippingInfo;
-    public Profile(ShippingInfo shippingInfo) {
+    public Profile(ShippingInfo shippingInfo) throws MissingShippingInfoException {
+        if (shippingInfo == null) {
+            throw new MissingShippingInfoException("ShippingInfo cannot be null");
+        }
         this.shippingInfo = shippingInfo;
     }
-
     public ShippingInfo getShippingInfo() {
         return shippingInfo;
     }
 
-    public void setShippingInfo(ShippingInfo shippingInfo) {
+    public void setShippingInfo(ShippingInfo shippingInfo) throws MissingShippingInfoException {
+        if (shippingInfo == null) {
+            throw new MissingShippingInfoException("ShippingInfo cannot be null");
+        }
         this.shippingInfo = shippingInfo;
     }
-
     // Added method to respect Law of Demeter
     public String getShippingCity() {
         return shippingInfo.getCity();
